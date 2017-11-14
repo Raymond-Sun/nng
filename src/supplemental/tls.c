14a261aaaae818d671fae820423da229d756e8a3
Garrett D'Amore <garrett@damore.org>
2017-11-14 13:33:11 -0800

    Rewrote code to use buffers, completed RECV logic.

--- src/supplemental/tls.c
+++ src/supplemental/tls.c
@@ -21,19 +21,25 @@
 
 #include "tls.h"
 
-// NNG_TLS_MAX_SEND_SIZE limits the amount of data we will send "immediately",
-// exerting backpressure if this size is exceeded.  This is also the size
-// of the maximum TCP window, so we should avoid introducing extra latency
-// caused by misalignment.
+// Implementation note.  This implementation buffers data between the TLS
+// encryption layer (mbedTLS) and the underlying TCP socket.  As a result,
+// there may be some additional latency caused by buffer draining and
+// refilling.  In the future we might want to investigate some kind of
+// double buffer policy to allow data to flow without entering a true
+// empty state.
+
+// NNG_TLS_MAX_SEND_SIZE limits the amount of data we will buffer for sending,
+// exerting backpressure if this size is exceeded.  The 16K is aligned to the
+// maximum TLS record size.
 #ifndef NNG_TLS_MAX_SEND_SIZE
-#define NNG_TLS_MAX_SEND_SIZE 65536
+#define NNG_TLS_MAX_SEND_SIZE 16384
 #endif
 
 // NNG_TLS_MAX_RECV_SIZE limits the amount of data we will receive in a single
 // operation.  As we have to buffer data, this drives the size of our
-// intermediary buffer.
+// intermediary buffer.  The 16K is aligned to the maximum TLS record size.
 #ifndef NNG_TLX_MAX_RECV_SIZE
-#define NNG_TLS_MAX_RECV_SIZE
+#define NNG_TLS_MAX_RECV_SIZE 16384
 #endif
 
 struct nni_tls {
@@ -46,11 +52,17 @@ struct nni_tls {
 	bool                recving;
 	bool                closed;
 	bool                hsdone;
-	char *              recvbuf;
-	int                 recvidx;
-	nni_list            sends;     // upper side sends
-	nni_list            recvs;     // upper recv aios
-	nni_aio *           handshake; // handshake aio (upper)
+	bool                tls_closed; // upper TLS layer closed
+	bool                tcp_closed; // underlying TCP buffer closed
+	uint8_t *           sendbuf;    // send buffer
+	int                 sendlen;    // amount of data in send buffer
+	int                 sendoff;    // offset of start of send data
+	uint8_t *           recvbuf;    // recv buffer
+	int                 recvlen;    // amount of data in recv buffer
+	int                 recvoff;    // offset of start of recv data
+	nni_list            sends;      // upper side sends
+	nni_list            recvs;      // upper recv aios
+	nni_aio *           handshake;  // handshake aio (upper)
 };
 
 struct nni_tls_config {
@@ -73,6 +85,7 @@ static void nni_tls_send_cb(void *);
 static void nni_tls_recv_cb(void *);
 
 static void nni_tls_do_send(nni_tls *);
+static void nni_tls_do_recv(nni_tls *);
 static void nni_tls_do_handshake(nni_tls *);
 
 static void
@@ -155,6 +168,10 @@ nni_tls_config_init(nni_tls_config **cpp, int mode)
 		return (rv);
 	}
 
+	// We *require* TLS v1.2 or newer, which is also known as SSL v3.3.
+	mbedtls_ssl_conf_min_version(&cfg->cfg_ctx,
+	    MBEDTLS_SSL_MAJOR_VERSION_3, MBEDTLS_SSL_MINOR_VERSION_3);
+
 #ifdef NNG_TLS_USE_CTR_DRBG
 	mbedtls_ctr_drbg_init(&cfg->rng_ctx);
 	rv = mbedtls_ctr_drbg_seed(
@@ -178,12 +195,18 @@ nni_tls_config_init(nni_tls_config **cpp, int mode)
 void
 nni_tls_fini(nni_tls *tp)
 {
+	// Shut it all down first.
+	nni_plat_tcp_pipe_close(tp->tcp);
 	nni_aio_stop(tp->tcp_send);
 	nni_aio_stop(tp->tcp_recv);
+
+	// And finalize / free everything.
+	nni_plat_tcp_pipe_fini(tp->tcp);
 	nni_aio_fini(tp->tcp_send);
 	nni_aio_fini(tp->tcp_recv);
 	mbedtls_ssl_free(&tp->ctx);
 	nni_mtx_fini(&tp->lk);
+	nni_free(tp->recvbuf, NNG_TLS_MAX_RECV_SIZE);
 	NNI_FREE_STRUCT(tp);
 }
 
@@ -219,11 +242,21 @@ nni_tls_init(nni_tls **tpp, nni_tls_config *cfg, nni_plat_tcp_pipe *tcp)
 	if ((tp = NNI_ALLOC_STRUCT(tp)) == NULL) {
 		return (NNG_ENOMEM);
 	}
+	if ((tp->recvbuf = nni_alloc(NNG_TLS_MAX_RECV_SIZE)) == NULL) {
+		NNI_FREE_STRUCT(tp);
+		return (NNG_ENOMEM);
+	}
 
 	nni_aio_list_init(&tp->sends);
 	nni_aio_list_init(&tp->recvs);
 	nni_mtx_init(&tp->lk);
 	mbedtls_ssl_init(&tp->ctx);
+
+	if ((rv = mbedtls_ssl_setup(&tp->ctx, &cfg->cfg_ctx)) != 0) {
+		rv = nni_tls_mkerr(rv);
+		nni_tls_fini(tp);
+		return (rv);
+	}
 
 	tp->tcp = tcp;
 
@@ -246,7 +279,6 @@ nni_tls_cancel(nni_aio *aio, int rv)
 		nni_aio_list_remove(aio);
 		nni_aio_finish_error(aio, rv);
 	}
-	// XXX: cancel the underlying TCP send/recv as well?
 	nni_mtx_unlock(&tp->lk);
 }
 
@@ -256,8 +288,10 @@ nni_tls_send_cb(void *ctx)
 {
 	nni_tls *tp = ctx;
 
+	// XXX: handling of errors...
+
 	nni_mtx_lock(&tp->lk);
-	tp->sending = 0;
+	tp->sending = false;
 	if (tp->handshake != NULL) {
 		nni_tls_do_handshake(tp);
 	} else if (!nni_list_empty(&tp->sends)) {
@@ -269,13 +303,27 @@ nni_tls_send_cb(void *ctx)
 static void
 nni_tls_recv_cb(void *ctx)
 {
-	nni_tls *tp = ctx;
+	nni_tls *tp  = ctx;
+	nni_aio *aio = tp->tcp_recv;
 
 	nni_mtx_lock(&tp->lk);
-	tp->recving = 0;
-	if (tp->handshake != NULL) {
+	tp->recving = false;
+	if (nni_aio_result(aio) != 0) {
+		// Mark the buffer closed, but allow pre-read data to
+		// be drained from it.
+		tp->tcp_closed = true;
+	} else {
+		NNI_ASSERT(tp->recvlen == 0);
+		NNI_ASSERT(tp->recvoff == 0);
+		tp->recvlen = nni_aio_count(aio);
+	}
+
+	// If we were closed (above), the upper layer will detect and
+	// react properly.  Otherwise the upper layer will consume data.
+	if (!tp->hsdone) {
 		nni_tls_do_handshake(tp);
-	} else if (!nni_list_empty(&tp->recvs)) {
+	}
+	if (tp->hsdone && (!nni_list_empty(&tp->recvs))) {
 		nni_tls_do_recv(tp);
 	}
 	nni_mtx_unlock(&tp->lk);
@@ -296,18 +344,24 @@ nni_tls_net_send(void *ctx, const unsigned char *buf, size_t len)
 	if (len > NNG_TLS_MAX_SEND_SIZE) {
 		len = NNG_TLS_MAX_SEND_SIZE;
 	}
+
 	// We should already be running with the pipe lock held,
 	// as we are running in that context.
+
 	if (tp->sending) {
 		return (MBEDTLS_ERR_SSL_WANT_WRITE);
 	}
-	if (tp->closed) {
+	if (tp->tcp_closed) {
 		return (MBEDTLS_ERR_NET_SEND_FAILED);
 	}
 
-	tp->sending                    = 1;
+	tp->sending = 1;
+	tp->sendlen = len;
+	tp->sendoff = 0;
+	memcpy(tp->sendbuf, buf, len);
+
 	tp->tcp_send->a_niov           = 1;
-	tp->tcp_send->a_iov[0].iov_buf = (char *) buf;
+	tp->tcp_send->a_iov[0].iov_buf = tp->sendbuf;
 	tp->tcp_send->a_iov[0].iov_len = len;
 	nni_aio_set_timeout(tp->tcp_send, -1); // No timeout.
 	nni_plat_tcp_pipe_send(tp->tcp, tp->tcp_send);
@@ -315,30 +369,42 @@ nni_tls_net_send(void *ctx, const unsigned char *buf, size_t len)
 }
 
 static int
-nni_tls_net_recv(void *ctx, const unsigned char *buf, size_t len)
+nni_tls_net_recv(void *ctx, unsigned char *buf, size_t len)
 {
 	nni_tls *tp = ctx;
 
 	// We should already be running with the pipe lock held,
 	// as we are running in that context.
-	if (tp->recving) {
-		return (MBEDTLS_ERR_SSL_WANT_READ);
-	}
-	if (tp->closed) {
+	if (tp->tcp_closed && tp->recvlen == 0) {
 		return (MBEDTLS_ERR_NET_SEND_FAILED);
 	}
 
-	tp->recving                    = 1;
-	tp->tcp_recv->a_niov           = 1;
-	tp->tcp_recv->a_iov[0].iov_buf = (char *) buf;
-	tp->tcp_recv->a_iov[0].iov_len = len;
-	nni_aio_set_timeout(tp->tcp_recv, -1); // No timeout.
-	nni_plat_tcp_pipe_recv(tp->tcp, tp->tcp_recv);
+	if (tp->recvlen == 0) {
+		len = MBEDTLS_ERR_SSL_WANT_READ;
+	} else {
+		if (len > tp->recvlen) {
+			len = tp->recvlen;
+		}
+		memcpy(buf, tp->recvbuf + tp->recvoff, len);
+		tp->recvoff += len;
+		tp->recvlen -= len;
+	}
+
+	if (tp->recvlen == 0 && tp->recving == 0) {
+		// If buffer is empty, then refill it.
+		tp->recving                    = true;
+		tp->recvoff                    = 0;
+		tp->tcp_recv->a_niov           = 1;
+		tp->tcp_recv->a_iov[0].iov_buf = tp->recvbuf;
+		tp->tcp_recv->a_iov[0].iov_len = NNG_TLS_MAX_RECV_SIZE;
+		nni_aio_set_timeout(tp->tcp_recv, -1); // No timeout.
+		nni_plat_tcp_pipe_recv(tp->tcp, tp->tcp_recv);
+	}
 	return (len);
 }
 
-// nni_tls_send is the exported send function.  It has a similar calling
-// convention as the platform TCP pipe.
+// nni_tls_send is the exported send function.  It has a similar
+// calling convention as the platform TCP pipe.
 void
 nni_tls_send(nni_tls *tp, nni_aio *aio)
 {
@@ -347,12 +413,7 @@ nni_tls_send(nni_tls *tp, nni_aio *aio)
 		nni_mtx_unlock(&tp->lk);
 		return;
 	}
-	if (!tp->hsdone) {
-		nni_mtx_unlock(&tp->lk);
-		nni_aio_finish_error(aio, NNG_ESTATE);
-		return;
-	}
-	if (tp->closed) {
+	if (tp->tls_closed) {
 		nni_mtx_unlock(&tp->lk);
 		nni_aio_finish_error(aio, NNG_ECLOSED);
 		return;
@@ -370,12 +431,7 @@ nni_tls_recv(nni_tls *tp, nni_aio *aio)
 		nni_mtx_unlock(&tp->lk);
 		return;
 	}
-	if (!tp->hsdone) {
-		nni_mtx_unlock(&tp->lk);
-		nni_aio_finish_error(aio, NNG_ESTATE);
-		return;
-	}
-	if (tp->closed) {
+	if (tp->tls_closed) {
 		nni_mtx_unlock(&tp->lk);
 		nni_aio_finish_error(aio, NNG_ECLOSED);
 		return;
@@ -385,19 +441,12 @@ nni_tls_recv(nni_tls *tp, nni_aio *aio)
 	nni_mtx_unlock(&tp->lk);
 }
 
-// nni_tls_handshake makes a non-blocking attempt to complete the handshake.
 void
 nni_tls_do_handshake(nni_tls *tp)
 {
-	int      rv;
-	nni_aio *aio = tp->handshake;
+	int rv;
 
-	if (aio == NULL) {
-		return;
-	}
-	if (tp->closed) {
-		tp->handshake = NULL;
-		nni_aio_finish_error(aio, NNG_ECLOSED);
+	if (tp->tls_closed) {
 		return;
 	}
 	rv = mbedtls_ssl_handshake(&tp->ctx);
@@ -409,108 +458,112 @@ nni_tls_do_handshake(nni_tls *tp)
 		return;
 	case 0:
 		// The handshake is done, yay!
-		tp->handshake = NULL;
-		tp->hsdone    = 1;
-		nni_aio_finish(aio, 0, 0);
+		tp->hsdone = true;
 		return;
 
 	default:
 		// Some other error occurred... would be nice to be
 		// able to diagnose it better.
-		tp->handshake = NULL;
-		nni_aio_finish_error(aio, nni_tls_mkerr(rv));
-		return;
+		tp->tls_closed = true;
+		nni_plat_tcp_pipe_close(tp->tcp);
+		tp->tcp_closed = true;
 	}
 }
 
 // nni_tls_do_send is called to try to send more data if we have not
-// yet completed the I/O.  It also completes any transactions that *have*
-// completed.  It must be called with the lock held.
+// yet completed the I/O.  It also completes any transactions that
+// *have* completed.  It must be called with the lock held.
 static void
 nni_tls_do_send(nni_tls *tp)
 {
 	nni_aio *aio;
 
 	while ((aio = nni_list_first(&tp->sends)) != NULL) {
-		int n;
+		int      n;
+		uint8_t *buf = NULL;
+		size_t   len = 0;
 
-		if (aio->a_niov == 0) {
-			// No more I/O to complete.
-			nni_aio_list_remove(aio);
-			nni_aio_finish(aio, 0, aio->a_count);
-			return;
-		}
-		if (aio->a_iov[0].iov_len == 0) {
-			for (int i = 0; i < aio->a_niov; i++) {
-				aio->a_iov[i] = aio->a_iov[i + 1];
+		for (int i = 0; i < aio->a_niov; i++) {
+			if (aio->a_iov[i].iov_len != 0) {
+				buf = aio->a_iov[i].iov_buf;
+				len = aio->a_iov[i].iov_len;
+				break;
 			}
-			aio->a_niov--;
+		}
+		if (len == 0 || buf == NULL) {
+			nni_aio_list_remove(aio);
+			nni_aio_finish_error(aio, NNG_EINVAL);
 			continue;
 		}
-		n = mbedtls_ssl_write(
-		    &tp->ctx, aio->a_iov[0].iov_buf, aio->a_iov[0].iov_len);
-		if (n >= 0) {
-			aio->a_iov[0].iov_buf += n;
-			aio->a_iov[0].iov_len -= n;
-			aio->a_count += n;
-			continue;
-		}
+
+		n = mbedtls_ssl_write(&tp->ctx, buf, len);
 
 		if (n == MBEDTLS_ERR_SSL_WANT_WRITE) {
-			// Cannot send any more data right now, wait for
-			// callback.
+			// Cannot send any more data right now, wait
+			// for callback.
 			return;
 		}
-
 		// Some other error occurred... this is not good.
 		// Want better diagnostics.
 		nni_aio_list_remove(aio);
-		nni_aio_finish_error(aio, nni_tls_mkerr(n));
+		if (n < 0) {
+			nni_aio_finish_error(aio, nni_tls_mkerr(n));
+		} else {
+			nni_aio_finish(aio, 0, n);
+		}
 	}
 }
 
-// nni_tls_do_recv is called to try to receive more data if we have not
-// yet completed the I/O.  It also completes any transactions that *have*
-// completed.  It must be called with the lock held.
 static void
 nni_tls_do_recv(nni_tls *tp)
 {
 	nni_aio *aio;
 
 	while ((aio = nni_list_first(&tp->recvs)) != NULL) {
-		int n;
+		int      n;
+		uint8_t *buf = NULL;
+		size_t   len = 0;
 
-		if (aio->a_niov == 0) {
-			// No more I/O to complete.
-			nni_aio_list_remove(aio);
-			nni_aio_finish(aio, 0, aio->a_count);
-			return;
-		}
-		if (aio->a_iov[0].iov_len == 0) {
-			for (int i = 0; i < aio->a_niov; i++) {
-				aio->a_iov[i] = aio->a_iov[i + 1];
+		for (int i = 0; i < aio->a_niov; i++) {
+			if (aio->a_iov[i].iov_len != 0) {
+				buf = aio->a_iov[i].iov_buf;
+				len = aio->a_iov[i].iov_len;
+				break;
 			}
-			aio->a_niov--;
+		}
+		if (len == 0 || buf == NULL) {
+			nni_aio_list_remove(aio);
+			nni_aio_finish_error(aio, NNG_EINVAL);
 			continue;
 		}
-		n = mbedtls_ssl_read(
-		    &tp->ctx, aio->a_iov[0].iov_buf, aio->a_iov[0].iov_len);
-		if (n >= 0) {
-			aio->a_iov[0].iov_buf += n;
-			aio->a_iov[0].iov_len -= n;
-			aio->a_count += n;
-			continue;
-		}
+		n = mbedtls_ssl_read(&tp->ctx, buf, len);
 
 		if (n == MBEDTLS_ERR_SSL_WANT_READ) {
-			// Cannot receive any more data right now, wait for
-			// callback.
+			// Cannot receive any more data right now, wait
+			// for callback.
 			return;
 		}
 
-		// Some other error occurred... this is not good.
-		// Want better diagnostics.
 		nni_aio_list_remove(aio);
-		nni_aio_finish_error(aio, nni_tls_mkerr(n));
+
+		if (n < 0) {
+			nni_aio_finish_error(aio, nni_tls_mkerr(n));
+		} else {
+			nni_aio_finish(aio, 0, n);
+		}
 	}
+}
+
+void
+nni_tls_close(nni_tls *tp)
+{
+	int rv;
+	nni_mtx_lock(&tp->lk);
+	tp->tls_closed = true;
+	// This may succeed, or it may fail.  Either way we don't care.
+	// Implementations that depend on close-notify to mean anything
+	// are broken by design, just like RFC.  Note that we do *NOT*
+	// close the TCP connection at this point.
+	(void) mbedtls_ssl_close_notify(&tp->ctx);
+	nni_mtx_unlock(&tp->lk);
 }
